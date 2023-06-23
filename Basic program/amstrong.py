c83f88773a37fd768a37da8c609a4aecf7afe5f4
sufiyan903 <131009052+sufiyan903@users.noreply.github.com>
2023-06-23 19:22:10 +0530

    Add files via upload

--- Basic program/amstrong.py
+++ Basic program/amstrong.py
@@ -1,22 +1,22 @@
-def is_armstrong(number):
-    num_str = str(number)
-    order = len(num_str)
-    sum = 0
-
-    for digit in num_str:
-        sum += int(digit) ** order
-
-    return sum == number
-
 lower_range = int(input("Enter lower range: "))
 upper_range = int(input("Enter upper range: "))
 
-armstrong_numbers = []
-
-for number in range(lower_range, upper_range + 1):
-    if is_armstrong(number):
-        armstrong_numbers.append(number)
-
 print("The Armstrong numbers are:")
-for number in armstrong_numbers:
-    print(number)
+
+for num in range(lower_range, upper_range + 1):
+    # Count the number of digits in the current number
+    order = len(str(num))
+    
+    # Initialize sum variable
+    sum = 0
+    
+    # Find the sum of the cubes of each digit
+    temp = num
+    while temp > 0:
+        digit = temp % 10
+        sum += digit ** order
+        temp //= 10
+    
+    # Check if the number is an Armstrong number
+    if num == sum:
+        print(num)
